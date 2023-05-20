b296e2a3a99b8b07d3135e665b10c49597cdf5e7
godfreypj <godfreypj@outlook.com>
2023-05-20 10:20:53 -0400

    added footer test

---
+++ src/components/Footer.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import Footer from './Footer';
+
+describe('Footer', () => {
+  it('renders the footer', () => {
+    const { getByText } = render(<Footer />);
+    const footerText = getByText(/godfreypj/i);
+    expect(footerText).toBeInTheDocument;
+  });
+});
