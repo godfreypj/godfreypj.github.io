706a6cdf2086c495129fbbfb21c97a67496b6879
godfreypj <godfreypj@outlook.com>
2023-05-21 07:46:42 -0400

    test fixes

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { JSDOM } from 'jsdom';
+// import { JSDOM } from 'jsdom';
 
 console.log('Setting up tests...');
 
@@ -13,12 +13,12 @@ jest.mock(
     props.children
 );
 
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
-global.window = dom.window as unknown as Window & typeof globalThis;
-global.document = dom.window.document;
-global.navigator = {
-  ...(global.navigator || {}),
-  userAgent: 'node.js',
-};
+// const dom = new JSDOM('<!doctype html><html><body></body></html>');
+// global.window = dom.window as unknown as Window & typeof globalThis;
+// global.document = dom.window.document;
+// global.navigator = {
+//   ...(global.navigator || {}),
+//   userAgent: 'node.js',
+// };
 // Set the test environment to 'jsdom'
 process.env.JEST_ENV = 'jsdom';
