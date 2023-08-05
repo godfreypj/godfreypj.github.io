1188c2f2724103438e5a2d1cbe74840ea1627195
godfreypj <godfreypj@outlook.com>
2023-08-05 09:04:51 -0400

    back to coverage

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,7 @@ module.exports = {
       roots: ['<rootDir>/src'],
       testEnvironment: 'jsdom',
       testMatch: ['**/__tests__/**/*.{js,jsx,ts,tsx}', '**/?(*.)+(spec|test).{js,jsx,ts,tsx}'],
-      collectCoverage: false,
+      collectCoverage: true,
       coverageReporters: ['html', 'lcov', 'text'],
       coverageThreshold: {
         global: {
