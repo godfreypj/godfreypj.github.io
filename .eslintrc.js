2a470eccfac6c6a703b60e0beee34fe82ad9426e
godfreypj <godfreypj@outlook.com>
2023-08-05 08:24:35 -0400

    took create react app typescript and removed react scripts and replaced it with craco

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// .eslintrc.js
+module.exports = {
+  extends: ['react-app', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+  },
+};
