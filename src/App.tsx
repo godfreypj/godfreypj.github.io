5bb6af05b3547e6b8bf815262d3a46a6c3efc845
godfreypj <godfreypj@outlook.com>
2023-08-18 18:16:41 -0400

    added a second page, About. added router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="about"
+          element={<About />}
+        />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
