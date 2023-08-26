cbccf576ca53ee91f99633dfd27324a2481536ba
godfreypj <godfreypj@outlook.com>
2023-08-26 09:55:12 -0400

    added about page, main title bar. desktop only

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,32 @@
+import selfie from '../../assets/selfie.jpg';
+import './About.css';
+
 export default function About() {
   return (
-    <div className="flex justify-center">
-      <div className="flex justify-center text-center h-1 m-0 artboard artboard-horizontal phone-1 bg-accent">
-        <article className="prose prose-xl">
-          Software Engineer, New England native & Navy veteran. Check out my skills and experience.
-        </article>
+    <div
+      className="flex flex-row lg:h-16 rounded bg-gradient-to-l
+                 from-accent to-neutral-focus hover:bg-gradient-to-r"
+      data-testid="about-bar"
+    >
+      <article
+        className="prose flex-grow font-bold lg:text-base lg:pt-4 lg:pl-20 p-0 m-0 text-xs"
+        data-testid="about-quote"
+      >
+        Software Engineer, New England native & Navy veteran.
+      </article>
+      <div
+        className="avatar flex-none self-center lg:ml-24 p-0 m-0"
+        data-testid="avatar"
+      >
+        <div
+          className="rounded-full w-28 ring ring-primary ring-offset-base-100 ring-offset-2"
+          data-testid="selfie-ring"
+        >
+          <img
+            src={selfie}
+            alt="selfie "
+          />
+        </div>
       </div>
     </div>
   );
