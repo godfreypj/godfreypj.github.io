6abaaf7217899eef58d00e4a1d9b1efd1daf4045
godfreypj <godfreypj@outlook.com>
2023-10-09 13:53:51 -0400

    maybe use an alternative to gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Header from './components/Header/Header';
